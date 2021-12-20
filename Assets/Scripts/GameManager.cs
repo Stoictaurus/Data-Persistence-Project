85dc9cb42ecb4998c92778b3dfdc43081fb969bd
Stoictaurus <stoictaurus@gmail.com>
2021-12-20 19:33:37 +0100

    Keep data between game starts
    Save best score and last player name (to pre-fill name at start) between game sessions.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,27 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor.UI;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Serializable]
+    private class GameData
+    {
+        public string playerName;
+        public string bestPlayer;
+        public int bestScore;
+    }
+
     public static GameManager Instance;
 
     public string PlayerName;
     public string BestPlayer;
     public int BestScore;
 
+    private string gameSavePath = "./gamedata.json";
     private void Awake()
     {
         if (Instance != null)
@@ -45,11 +56,24 @@ public class GameManager : MonoBehaviour
     
     private void RestorePersistentData()
     {
-        
+        if (File.Exists(gameSavePath))
+        {
+            var json = File.ReadAllText(gameSavePath);
+            var data = JsonUtility.FromJson<GameData>(json);
+            PlayerName = data.playerName;
+            BestPlayer = data.bestPlayer;
+            BestScore = data.bestScore;
+        }
     }
 
     private void SavePersistentData()
     {
+        var data = new GameData();
+        data.bestPlayer = BestPlayer;
+        data.bestScore = BestScore;
+        data.playerName = PlayerName;
         
+        var json = JsonUtility.ToJson(data);
+        File.WriteAllText(gameSavePath, json);
     }
 }
